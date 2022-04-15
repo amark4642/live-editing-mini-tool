8c64746535df71fb321e9399f8ea340fa9837c82
AMAR KUMAR CHOUDHARY <amark4642@gmail.com>
2022-04-15 22:44:05 +0530

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import '../styles/App.css';
+// import { Preview } from './Preview';
+const App = () => {
 
-function App() {
+  const [contentInput, setContentInput] = useState("hello newton");
+  const [fontSizeInput, setFontSizeInput] = useState(10);
+  const [paddingInput, setPaddingInput] = useState(5);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main">
+      <div>
+        <input
+          type="text"
+          id="contentInput"
+          value={contentInput}
+          onChange={(e) => setContentInput(e.target.value)}
+        />
+        <br />
+        <input
+          type="number"
+          id="fontSizeInput"
+          value={fontSizeInput}
+          onChange={(e) => setFontSizeInput(e.target.value)}
+        />
+        <br />
+        <input
+          type="number"
+          id="paddingInput"
+          value={paddingInput}
+          onChange={(e) => setPaddingInput(e.target.value)}
+        />
+      </div>
+      <Preview content={contentInput} fontSize={`${fontSizeInput}px`} padding={`${paddingInput}px`} />
     </div>
-  );
+  )
 }
+
+function Preview(props) {
+  console.log("test "+ props.content);
+  return (
+    <div style={{ fontSize: `${props.fontSize}`, padding: `${props.padding}`}}>
+      {props.content}
+    </div>
+  )
+}
+
 
 export default App;
